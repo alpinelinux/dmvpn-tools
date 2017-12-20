847297c1b1ca5b2e633187dec67612b07a2b0d44
Kaarle Ritvanen <kaarle.ritvanen@datakunkku.fi>
2017-12-20 14:35:06 +0200

    hub domain name extension

--- dmvpn.lua
+++ dmvpn.lua
@@ -3,4 +3,6 @@ Copyright (c) 2014-2018 Kaarle Ritvanen
 See LICENSE file for license details
 ]]--
 
-return {OID_IS_HUB='1.3.6.1.4.1.31536.1.1'}
+local base = '1.3.6.1.4.1.31536.1.'
+
+return {OID_IS_HUB=base..'1', OID_HUB_HOSTS=base..'2'}
