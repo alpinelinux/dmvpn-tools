23d4883eb637bb1387f22e0ab6c94f43b105127a
Kaarle Ritvanen <kaarle.ritvanen@datakunkku.fi>
2017-12-28 20:32:03 +0200

    add missing dependency: lua5.2-posix

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 sudo apk add --virtual .dmvpn-ca-deps lua5.2 lua5.2-lyaml lua5.2-ossl \
-	lua-asn1 lua5.2-sql-sqlite3 lua5.2-stringy
+	lua5.2-posix lua5.2-sql-sqlite3 lua5.2-stringy lua-asn1
