1dba01dd8267011eae1ae705faced2858173bf95
Kaarle Ritvanen <kaarle.ritvanen@datakunkku.fi>
2017-12-28 18:28:49 +0200

    setup script

--- dependencies.sh
+++ dependencies.sh
@@ -8,5 +8,6 @@
 # See LICENSE file for license details
 
 sudo apk add -t .dmvpn-tools-deps \
-	lua5.2 lua5.2-cqueues lua5.2-lyaml lua5.2-ossl lua5.2-posix \
-	lua5.2-sql-sqlite3 lua5.2-stringy lua5.2-struct lua-asn1
+	augeas bind-tools lua5.2 lua5.2-cqueues lua5.2-lyaml lua5.2-ossl \
+	lua5.2-posix lua5.2-sql-sqlite3 lua5.2-stringy lua5.2-struct lua-asn1 \
+	quagga strongswan tunnel
