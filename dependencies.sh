6a39747dbd04a371cf113a3314dd709765ac2795
Kaarle Ritvanen <kaarle.ritvanen@datakunkku.fi>
2018-02-15 16:49:52 +0200

    import nhrp-events.lua

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,5 @@
-#!/bin/sh -e
+#!/bin/sh
 
-sudo apk add --virtual .dmvpn-ca-deps lua5.2 lua5.2-lyaml lua5.2-ossl \
-	lua5.2-posix lua5.2-sql-sqlite3 lua5.2-stringy lua-asn1
+sudo apk add -t .dmvpn-tools-deps \
+	lua5.2 lua5.2-cqueues lua5.2-lyaml lua5.2-ossl lua5.2-posix \
+	lua5.2-sql-sqlite3 lua5.2-stringy lua5.2-struct lua-asn1
