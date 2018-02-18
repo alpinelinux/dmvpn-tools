b4942b772369dde1e6602923d2d12250c711cd67
Kaarle Ritvanen <kaarle.ritvanen@datakunkku.fi>
2018-02-18 22:14:29 +0200

    update IPR notices

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Install dependencies for dmvpn-tools
+#
+# Copyright (c) 2014-2018 Kaarle Ritvanen
+# Copyright (c) 2017 Natanael Copa
+#
+# See LICENSE file for license details
+
 sudo apk add -t .dmvpn-tools-deps \
 	lua5.2 lua5.2-cqueues lua5.2-lyaml lua5.2-ossl lua5.2-posix \
 	lua5.2-sql-sqlite3 lua5.2-stringy lua5.2-struct lua-asn1
--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/sh -e
 
-sh ./dependencies.sh
+# Create test database for dmvpn-ca
+#
+# Copyright (c) 2014-2018 Kaarle Ritvanen
+# Copyright (c) 2017 Natanael Copa
+#
+# See LICENSE file for license details
+
+./dependencies.sh
 
 while read cmd; do
 	if [ "$cmd" ]; then
